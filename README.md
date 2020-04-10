52482a469fc2eb7d4ad9d390bdbf8bb593b0f7d7
Olga Sadova <sadovaolga1@gmail.com>
2020-04-10 09:56:47 -0700

    added more picture

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # HerokuNO
+
+
+## Created an application that can to write, save, and delete notes. This application  uses an express backend and save and retrieve note data from a JSON file.
+
+
+
+<img src="./Picture1.jpg" alt="Picture" />
--- index.js
+++ index.js
@@ -1,241 +1,98 @@
-// ==============================================================================
-// DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
-// ==============================================================================
-
-/*const express = require("express");
-//const router = express.Router();
-//const express = require("express");
-
-// ==============================================================================
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
-// ==============================================================================
-
-// Tells node that we are creating an "express" server
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 const app = express();
 
-// Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+let notesData = [];
 
-// Sets up the Express app to handle data parsing
-
-app.use(express.static("public"))
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
-require("./routes/apiRoute")(app);
-require("./routes/htmlRoute")(app);
-// const htmlRoutes = require("./routes/htmlRoute");
-// app.use(htmlRoute);
-// //DATA ROUTES
-// const apiRoutes = require("./routes/apiRoute");
-// app.use("/api", apiRoute);
-
-// =============================================================================
-// LISTENER
-// The below code effectively "starts" our server
-// =============================================================================
-
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-});*/
-const { v1: uuidv1 } = require('uuid');
-
-
-
-var path = require("path");
-
-var express = require("express");
-
-const fs = require("fs");
-
-const util = require("util");
-
-const database = "db/db.json"
-
-var app = express();
-
-var PORT = process.env.PORT || 3000;
-
-const readFileAsync = util.promisify(fs.readFile);
-
-const writeFileAsync = util.promisify(fs.writeFile);
-
-
-
-
-
-app.use(express.static("public"))
-
-app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "Develop/public")));
 
 
 
 
 
+app.get("/api/notes", function(err, res) {
+    try {
+        notesData = fs.readFileSync("Develop/db/db.json", "utf8");
+        notesData = JSON.parse(notesData);
+    } catch (err) {
+        console.log("\n error (in app.get.catch):");
+        console.log(err);
+    }
+    res.json(notesData);
+});
 
-app.get("/api/notes", async function(req, res) {
 
+app.post("/api/notes", function(req, res) {
     try {
 
+        notesData = fs.readFileSync("Develop/db/db.json", "utf8");
+        console.log(notesData);
+        notesData = JSON.parse(notesData);
+        req.body.id = notesData.length;
+        notesData.push(req.body);
+        notesData = JSON.stringify(notesData);
+        fs.writeFile("Develop/db/db.json", notesData, "utf8", function(err) {
+
+            if (err) throw err;
+        });
+
+        res.json(JSON.parse(notesData));
 
 
-        const data = await readFileAsync(database, "utf8");
-
-        res.json(JSON.parse(data))
-
-
-
-    } catch {
-
-        console.log(err)
-
+    } catch (err) {
+        throw err;
+        console.log(err);
     }
-
 });
 
 
 
-
-
-app.post("/api/notes", async function(req, res) {
-
+app.delete("/api/notes/:id", function(req, res) {
     try {
 
-        let note = req.body;
+        notesData = fs.readFileSync("Develop/db/db.json", "utf8");
+        notesData = JSON.parse(notesData);
+        notesData = notesData.filter(function(note) {
+            return note.id != req.params.id;
+        });
 
-        let data = await readFileAsync(database, "utf8");
+        notesData = JSON.stringify(notesData);
 
-        let noteArr = JSON.parse(data);
+        fs.writeFile("Develop/db/db.json", notesData, "utf8", function(err) {
 
-
-
-        note.id = uuidv1();
-
-
-
-        noteArr.push(note)
-
+            if (err) throw err;
+        });
 
 
-        let strArr = JSON.stringify(noteArr, null, 4)
-
-
-
-        await writeFileAsync(database, strArr);
-
-        res.json(note)
-
-
-
-
-
-
-
-
-
-
-
-    } catch {
-
-        console.log("error")
-
+        res.send(JSON.parse(notesData));
+    } catch (err) {
+        throw err;
+        console.log(err);
     }
-
-
-
 });
 
 
 
-
-
-
-
-app.delete("/api/notes/:id", async function(req, res) {
-
-    try {
-
-
-
-        let data = await readFileAsync(database, "utf8");
-
-
-
-        let noteID = req.params.id
-
-        let noteArr = JSON.parse(data)
-
-
-
-        for (let i = 0; i < noteArr.length; i++) {
-
-            if (noteArr[i].id === noteID) {
-
-                noteArr.splice(i, 1);
-
-            }
-
-        }
-
-
-
-
-
-        let strArr = JSON.stringify(noteArr, null, 4)
-
-
-
-        await writeFileAsync(database, strArr);
-
-        res.json({ ok: true })
-
-
-
-    } catch {
-
-        console.log("error")
-
-    }
-
-});
-
-
-
-
-
+// Web page when the Get started button is clicked
 app.get("/notes", function(req, res) {
-
-    res.sendFile(path.join(__dirname, "/notes.html"));
-
+    res.sendFile(path.join(__dirname, "Develop/public/notes.html"));
 });
 
-
-
-
-
+// If no matching route is found default to home
 app.get("*", function(req, res) {
-
-    res.sendFile(path.join(__dirname, "/index.html"));
-
+    res.sendFile(path.join(__dirname, "Develop/public/index.html"));
 });
 
+app.get("/api/notes", function(req, res) {
+    return res.sendFile(path.json(__dirname, "Develop/db/db.json"));
+});
 
-
-
-
-
-
+// Start the server on the port
 app.listen(PORT, function() {
-
-    console.log("App listening on PORT: " + PORT);
-
+    console.log("SERVER IS LISTENING: " + PORT);
 });
